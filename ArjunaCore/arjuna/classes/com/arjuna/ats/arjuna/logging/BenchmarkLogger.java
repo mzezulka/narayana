ac7ecd951cbc4ba3a7c377bc415233571bb10c7c
Miloslav Žežulka <mzezulka@dhcp-10-40-5-80.brq.redhat.com>
2019-12-11 10:01:09 +0100

    Add stdout appender.

---
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/logging/BenchmarkLogger.java
@@ -0,0 +1,15 @@
+package com.arjuna.ats.arjuna.logging;
+
+import java.util.UUID;
+import org.apache.log4j.Logger;
+
+public final class BenchmarkLogger {
+    private static final Logger LOGGER = Logger.getLogger(BenchmarkLogger.class.getName());
+    private static final String MSG = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
+            + " do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
+            + "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+    public static void logMessage() {
+        LOGGER.info(UUID.randomUUID() + " " + MSG);
+    }
+}
