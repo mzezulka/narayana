e9acd50f2e60244445eac3897c1499a3fd8abc04
Miloslav Žežulka <mzezulka@dhcp-10-40-5-80.brq.redhat.com>
2019-12-13 13:52:27 +0100

    Remove Mockito code.

--- ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/TracingTest.java
+++ ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/TracingTest.java
@@ -13,57 +13,21 @@ import io.opentracing.util.GlobalTracer;
 
 public class TracingTest {
 
-//    @Mock
-//    private Tracer mockTracer;
-//
-//    @Mock
-//    private Tracer.SpanBuilder mockSpanBuilder;
-//
-//    @Mock
-//    private ScopeManager mockScopeManager;
-//
-//    @Mock
-//    private Scope mockScope;
-//
-//    @Mock
-//    private Span mockSpan;
-//
-//    @Mock
-//    private SpanContext mockSpanContext;
-
     @Before
     public void before() {
-//      MockitoAnnotations.initMocks(this);
-//      when(mockTracer.buildSpan(anyString())).thenReturn(mockSpanBuilder);
-//      when(mockSpanBuilder.asChildOf(any(SpanContext.class))).thenReturn(mockSpanBuilder);
-//      when(mockSpanBuilder.start()).thenReturn(mockSpan);
-//      when(mockScopeManager.activate(mockSpan)).thenReturn(mockScope);
-//      when(mockTracer.scopeManager()).thenReturn(mockScopeManager);
-//      when(mockScopeManager.activeSpan()).thenReturn(mockSpan);
-//      when(mockSpanBuilder.withTag(anyString(), anyString())).thenReturn(mockSpanBuilder);
-//      when(mockSpan.context()).thenReturn(mockSpanContext);
-    }
-
-    @Test
-    public void begin() {
-        MockTracer mt = new MockTracer();
-        GlobalTracer.registerIfAbsent(mt);
-        AtomicAction a = new AtomicAction();
-        a.begin();
-        a.commit();
-        assertThat(mt.finishedSpans().size()).isEqualTo(2);
     }
 
     @Test
     public void commit() {
+        MockTracer mt = new MockTracer();
         BasicRecord record1 = new BasicRecord();
         BasicRecord record2 = new BasicRecord();
-
+        GlobalTracer.registerIfAbsent(mt);
         AtomicAction a = new AtomicAction();
         a.begin();
         a.add(record1);
         a.add(record2);
         a.commit();
+        assertThat(mt.finishedSpans().size()).isEqualTo(2);
     }
-
 }
