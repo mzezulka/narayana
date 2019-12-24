fa41b7e30e15e901d7b19c17da64f9bc98ce6424
Miloslav Žežulka <mzezulka@localhost.localdomain>
2019-12-24 13:29:01 +0100

    Refactoring and test decomposition.

--- ArjunaJTA/jta/tests/classes/com/arjuna/ats/internal/jta/opentracing/TracingTest.java
+++ ArjunaJTA/jta/tests/classes/com/arjuna/ats/internal/jta/opentracing/TracingTest.java
@@ -17,6 +17,7 @@ import com.hp.mwtests.ts.jta.common.XACreator;
 import io.narayana.tracing.names.SpanName;
 import io.opentracing.mock.MockSpan;
 import io.opentracing.mock.MockTracer;
+import io.opentracing.tag.Tags;
 import io.opentracing.util.GlobalTracer;
 
 public class TracingTest {
@@ -35,6 +36,27 @@ public class TracingTest {
         testTracer.reset();
     }
 
+    /**
+     *  Basic commit intermediated by JTA which makes use of two XAResources.
+     *  Spans are reported in the following order:
+     *
+     *    span index   id    parentid        op name
+     *    in 'spans'
+     *    ================================================
+     *        0        10       3         "Enlistment"
+     *        1        11       3         "Enlistment"
+     *        2         3       2         "XAResource Enlistments"
+     *        3         5       4         "Branch Prepare"
+     *        4         6       4         "Branch Prepare"
+     *        5         4       2         "Global Prepare"
+     *        6         8       7         "Branch Commit"
+     *        7         9       7         "Branch Commit"
+     *        8         7       2         "Global Commit"
+     *        9         2       0         "Transaction"
+     *  Please note that specific ids of spans may vary based on the mock implementation
+     *  of the tracer, the important bit is the relations between spans. This holds true
+     *  for any other test.
+     */
     private void jtaTwoPhaseCommit() throws Exception {
         String xaResource = "com.hp.mwtests.ts.jta.common.DummyCreator";
         XACreator creator = (XACreator) Thread.currentThread().getContextClassLoader().loadClass(xaResource).newInstance();
@@ -46,6 +68,21 @@ public class TracingTest {
         tm.commit();
     }
 
+    /**
+     *  User initiated JTA abort, making use of two XAResources.
+     *  Spans are reported in the following order:
+     *
+     *    span index   id    parentid        op name
+     *    in 'spans'
+     *    ================================================
+     *        0        6        5         "Enlistment"
+     *        1        7        5         "Enlistment"
+     *        2        5        4         "XAResource Enlistments"
+     *        3        9        8         "Branch Rollback"
+     *        4       10        8         "Branch Rollback"
+     *        5        8        4         "Global Abort - User Initiated"
+     *        6        4        0         "Transaction"
+     */
     private void jtaRollback() throws Exception {
         String xaResource = "com.hp.mwtests.ts.jta.common.DummyCreator";
         XACreator creator = (XACreator) Thread.currentThread().getContextClassLoader().loadClass(xaResource).newInstance();
@@ -61,34 +98,32 @@ public class TracingTest {
         return Arrays.asList(ops).stream().map(s -> s.toString()).collect(Collectors.toList());
     }
 
-    /**
-     *  Basic commit with two records of instance com.hp.mwtests.ts.arjuna.resources.BasicRecord.
-     *  Spans are reported in the following order:
-     *
-     *    span index   id    parentid        op name
-     *    in 'spans'
-     *    ================================================
-     *        0        10       3         "Enlistment"
-     *        1        11       3         "Enlistment"
-     *        2         3       2         "XAResource Enlistments"
-     *        3         5       4         "Branch Prepare"
-     *        4         6       4         "Branch Prepare"
-     *        5         4       2         "Global Prepare"
-     *        6         8       7         "Branch Rollback"
-     *        7         9       7         "Branch Rollback"
-     *        8         7       2         "Global Abort"
-     *        9         2       0         "Transaction"
-     *  Please note that specific ids of spans may vary based on the mock implementation
-     *  of the tracer, the important bit is the relations between spans.
+    /*
+     * Retrieve the root span which must always sit at the very end of the collection (because
+     * spans are reported in a postorder fashion.
      */
+    private MockSpan getRootSpanFrom(List<MockSpan> spans) {
+        return spans.get(spans.size()-1);
+    }
+
     @Test
     public void commitAndCheckRootSpan() throws Exception {
         jtaTwoPhaseCommit();
         List<MockSpan> spans = testTracer.finishedSpans();
-
-        MockSpan root = spans.get(9);
+        MockSpan root = getRootSpanFrom(spans);
         // parent id 0 == no parent exists == the root of a trace
         assertThat(root.parentId()).isEqualTo(0);
+        assertThat((boolean) root.tags().get(Tags.ERROR.getKey())).isFalse();
+    }
+
+    @Test
+    public void commitAndCheckChildren() throws Exception {
+        jtaTwoPhaseCommit();
+        List<MockSpan> spans = testTracer.finishedSpans();
+        MockSpan root = getRootSpanFrom(spans);
+        //                                   tx-root
+        //                              /       |         \
+        //       "XAResource Enlistments" "Global Prepare" "Global Abort - User Initiated"
         assertThat(Arrays.asList(spans.get(8).parentId(),
                                  spans.get(5).parentId(),
                                  spans.get(2).parentId()))
@@ -108,14 +143,33 @@ public class TracingTest {
                                                                SpanName.LOCAL_COMMIT,
                                                                SpanName.GLOBAL_COMMIT,
                                                                SpanName.TX_ROOT);
-        assertThat(testTracer.finishedSpans().size()).isEqualTo(opNamesExpected.size());
-        List<String> opNames = testTracer.finishedSpans().stream().map(s -> s.operationName()).collect(Collectors.toList());
+        List<MockSpan> spans = testTracer.finishedSpans();
+        assertThat(spans.size()).isEqualTo(opNamesExpected.size());
+        List<String> opNames = spans.stream().map(s -> s.operationName()).collect(Collectors.toList());
         assertThat(opNames).isEqualTo(opNamesExpected);
     }
 
     @Test
-    public void abortAndCheckSpans() throws Exception {
+    public void abortAndCheckRootSpan() throws Exception {
         jtaRollback();
+        List<MockSpan> spans = testTracer.finishedSpans();
+        MockSpan root = getRootSpanFrom(spans);
+        // parent id 0 == no parent exists == the root of a trace
+        assertThat(root.parentId()).isEqualTo(0);
+        // this is *user-initiated* abort
+        assertThat((boolean) root.tags().get(Tags.ERROR.getKey())).isFalse();
+        }
+
+    @Test
+    public void abortAndCheckChildren() throws Exception {
+        jtaRollback();
+        List<MockSpan> spans = testTracer.finishedSpans();
+        MockSpan root = getRootSpanFrom(spans);
+        //                               tx-root
+        //                              /      \
+        //       "XAResource Enlistments"      "Global Commit"
+        assertThat(Arrays.asList(spans.get(2).parentId(), spans.get(5).parentId()))
+                  .containsOnly(root.context().spanId());
     }
 
     @Test
@@ -128,8 +182,9 @@ public class TracingTest {
                                                                SpanName.LOCAL_ROLLBACK,
                                                                SpanName.GLOBAL_ABORT_USER,
                                                                SpanName.TX_ROOT);
-        assertThat(testTracer.finishedSpans().size()).isEqualTo(opNamesExpected.size());
-        List<String> opNames = testTracer.finishedSpans().stream().map(s -> s.operationName()).collect(Collectors.toList());
+        List<MockSpan> spans = testTracer.finishedSpans();
+        assertThat(spans.size()).isEqualTo(opNamesExpected.size());
+        List<String> opNames = spans.stream().map(s -> s.operationName()).collect(Collectors.toList());
         assertThat(opNames).isEqualTo(opNamesExpected);
     }
 }
