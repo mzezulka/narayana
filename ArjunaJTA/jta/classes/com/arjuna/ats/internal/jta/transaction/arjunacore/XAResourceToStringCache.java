1df4ee9333ce1a7b641ab4695def4352e68ea81f
Miloslav Žežulka <mzezulka@localhost.localdomain>
2020-02-07 13:47:11 +0100

    Revert back.

--- ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/XAResourceToStringCache.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/XAResourceToStringCache.java
@@ -1,7 +1,6 @@
 package com.arjuna.ats.internal.jta.transaction.arjunacore;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.transaction.xa.XAResource;
@@ -10,14 +9,11 @@ public class XAResourceToStringCache {
 
     private static final Map<XAResource, String> CACHE = new ConcurrentHashMap<>();
 
-
     public static String get(XAResource xaRes) {
-        Objects.requireNonNull(xaRes);
         return CACHE.computeIfAbsent(xaRes, x -> x.toString());
     }
 
     public static void purge(XAResource xaRes) {
-        Objects.requireNonNull(xaRes);
-        if (CACHE.remove(xaRes) == null) throw new IllegalArgumentException();
+        CACHE.remove(xaRes);
     }
 }
