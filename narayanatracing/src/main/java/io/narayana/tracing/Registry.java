55860e74303338f2332c54172438336fcde34dd9
Miloslav Žežulka <mzezulka@localhost.localdomain>
2020-03-04 08:41:21 +0100

    Update javadoc (mainly span -> T).

--- narayanatracing/src/main/java/io/narayana/tracing/Registry.java
+++ narayanatracing/src/main/java/io/narayana/tracing/Registry.java
@@ -12,8 +12,8 @@ public abstract class Registry<T> {
 
     /**
      * @throws IllegalArgumentException id is null
-     * @param id id of the span (i.e. transaction ID), must not be null
-     * @return {@code Span} from the registry of root spans
+     * @param id id of {@code T}, must not be null
+     * @return {@code T} from the registry
      */
     public Optional<T> get(String id) {
         if(!TRACING_ACTIVATED) return Optional.empty();
@@ -24,10 +24,10 @@ public abstract class Registry<T> {
 
     /**
      * Same as {@link #get(String) get} but with the effect of
-     * removing the span from the registry.
+     * removing {@code T} from the registry.
      *
-     * @param id id of the span (i.e. transaction ID), must not be null
-     * @return the deleted {@code Span} if tracing is activated, empty otherwise
+     * @param id id of {@code T}, must not be null
+     * @return the deleted {@code T} wrapped in optional, Optional.empty otherwise
      */
     public Optional<T> remove(String id) {
         if(!TRACING_ACTIVATED) return Optional.empty();
@@ -37,18 +37,19 @@ public abstract class Registry<T> {
     }
 
     /**
-     * Insert {@code span} into registry of root spans. If an entry in this registry exists,
-     * i.e. entry with id {@code id} is already present, this method does not
-     * complete successfully. All parameters must be nonnull.
+     * Insert {@code T} into this registry. If an entry in this registry exists,
+     * i.e. entry with id {@code id} is already present, this throws an exception.
      *
-     * @throws IllegalArgumentException any parameter is null or {@code span} with
+     * All parameters must be nonnull.
+     *
+     * @throws IllegalArgumentException any parameter is null or {@code T} with
      *                                  id {@code id} is already registered.
      */
-    public void insert(String id, T span) {
+    public void insert(String id, T t) {
         if(!TRACING_ACTIVATED) return;
         Objects.requireNonNull(id);
-        Objects.requireNonNull(span);
-        if (store.put(id, span) != null)
+        Objects.requireNonNull(t);
+        if (store.put(id, t) != null)
             throw new IllegalArgumentException(
                     String.format("There is already an entry with id %s in the registry.", id));
     }
