882cf8340cd4568c5b7d25a030924bcf9b25c311
Miloslav Žežulka <mzezulka@redhat.com>
2020-06-25 11:54:18 +0200

    replace tabs -> spaces

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/common/ObjectStoreEnvironmentBeanMBean.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/common/ObjectStoreEnvironmentBeanMBean.java
@@ -20,13 +20,15 @@
  */
 package com.arjuna.ats.arjuna.common;
 
+import com.arjuna.ats.arjuna.objectstore.jdbc.JDBCAccess;
+
 /**
- * A JMX MBean interface containing configuration for the objectstore and
- * various implementations thereof.
+ * A JMX MBean interface containing configuration for the objectstore and various implementations thereof.
  *
  * @author Jonathan Halliday (jonathan.halliday@redhat.com)
  */
-public interface ObjectStoreEnvironmentBeanMBean {
+public interface ObjectStoreEnvironmentBeanMBean
+{
     int getCacheStoreSize();
 
     boolean isCacheStoreSync();
@@ -62,45 +64,48 @@ public interface ObjectStoreEnvironmentBeanMBean {
     long getTxLogSize();
 
     long getPurgeTime();
+    
+	/**
+	 * Get the JDBCAccess details.
+	 */
+	public String getJdbcAccess();
 
-    /**
-     * Get the JDBCAccess details.
-     */
-    public String getJdbcAccess();
+	/**
+	 * Sets the instance of JDBCAccess
+	 * 
+	 * @param connectionDetails
+	 *            an Object that provides JDBCAccess, or null.
+	 */
+	public void setJdbcAccess(String connectionDetails);
 
-    /**
-     * Sets the instance of JDBCAccess
-     *
-     * @param connectionDetails an Object that provides JDBCAccess, or null.
-     */
-    public void setJdbcAccess(String connectionDetails);
+	/**
+	 * Get the table prefix
+	 * 
+	 * @return The prefix to apply to the table
+	 */
+	public String getTablePrefix();
 
-    /**
-     * Get the table prefix
-     *
-     * @return The prefix to apply to the table
-     */
-    public String getTablePrefix();
+	/**
+	 * Set the table prefix
+	 * 
+	 * @param tablePrefix
+	 *            A prefix to use on the tables
+	 */
+	public void setTablePrefix(String tablePrefix);
 
-    /**
-     * Set the table prefix
-     *
-     * @param tablePrefix A prefix to use on the tables
-     */
-    public void setTablePrefix(String tablePrefix);
+	/**
+	 * Should the store drop the table
+	 * 
+	 * @return Whether to drop the table
+	 */
+	public boolean getDropTable();
 
-    /**
-     * Should the store drop the table
-     *
-     * @return Whether to drop the table
-     */
-    public boolean getDropTable();
-
-    /**
-     * Set whether to drop the table.
-     *
-     * @param dropTable Drop the table
-     */
-    public void setDropTable(boolean dropTable);
+	/**
+	 * Set whether to drop the table.
+	 * 
+	 * @param dropTable
+	 *            Drop the table
+	 */
+	public void setDropTable(boolean dropTable);
 
 }
